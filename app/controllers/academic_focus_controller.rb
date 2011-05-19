b0beba1329dca851cce80915e7c76c6821a37e2a
Cameron <cameron29w@gmail.com>
2011-05-18 23:25:35 -0600

    Added static pages controller.  Index page that contains links to all the models.

--- app/controllers/academic_focus_controller.rb
+++ app/controllers/academic_focus_controller.rb
@@ -2,7 +2,7 @@ class AcademicFocusController < ApplicationController
   # GET /academic_focus
   # GET /academic_focus.xml
   def index
-    @academic_focus = AcademicFocu.all
+    @academic_focus = AcademicFocuses.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,42 +13,42 @@ class AcademicFocusController < ApplicationController
   # GET /academic_focus/1
   # GET /academic_focus/1.xml
   def show
-    @academic_focu = AcademicFocu.find(params[:id])
+    @academic_focus = AcademicFocuses.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @academic_focu }
+      format.xml  { render :xml => @academic_focus }
     end
   end
 
   # GET /academic_focus/new
   # GET /academic_focus/new.xml
   def new
-    @academic_focu = AcademicFocu.new
+    @academic_focus = AcademicFocuses.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @academic_focu }
+      format.xml  { render :xml => @academic_focus }
     end
   end
 
   # GET /academic_focus/1/edit
   def edit
-    @academic_focu = AcademicFocu.find(params[:id])
+    @academic_focus = AcademicFocuses.find(params[:id])
   end
 
   # POST /academic_focus
   # POST /academic_focus.xml
   def create
-    @academic_focu = AcademicFocu.new(params[:academic_focu])
+    @academic_focus = AcademicFocuses.new(params[:academic_focus])
 
     respond_to do |format|
-      if @academic_focu.save
-        format.html { redirect_to(@academic_focu, :notice => 'Academic focu was successfully created.') }
-        format.xml  { render :xml => @academic_focu, :status => :created, :location => @academic_focu }
+      if @academic_focus.save
+        format.html { redirect_to(@academic_focus, :notice => 'Academic focus was successfully created.') }
+        format.xml  { render :xml => @academic_focus, :status => :created, :location => @academic_focus }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @academic_focu.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @academic_focus.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -56,15 +56,15 @@ class AcademicFocusController < ApplicationController
   # PUT /academic_focus/1
   # PUT /academic_focus/1.xml
   def update
-    @academic_focu = AcademicFocu.find(params[:id])
+    @academic_focus = AcademicFocuses.find(params[:id])
 
     respond_to do |format|
-      if @academic_focu.update_attributes(params[:academic_focu])
-        format.html { redirect_to(@academic_focu, :notice => 'Academic focu was successfully updated.') }
+      if @academic_focus.update_attributes(params[:academic_focus])
+        format.html { redirect_to(@academic_focus, :notice => 'Academic focus was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @academic_focu.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @academic_focus.errors, :status => :unprocessable_entity }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,99 +1,28 @@
 Internships::Application.routes.draw do
   
   resources :academic_contacts
-  
   resources :academic_focuses
-  
   resources :academic_focus_types
-  
   resources :colleges
-
   resources :countries
-  
   resources :departments
-
   resources :fields
-  
   resources :financial_assistance_options
-  
   resources :financial_assistance_option_types
-  
   resources :industries
-  
   resources :internships
-  
   resources :internship_instances
-  
   resources :languages
-  
   resources :locations
-  
   resources :providers
-  
   resources :provider_contacts
-
   resources :provider_types
-  
   resources :semesters
-  
   resources :states
-  
   resources :students
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
