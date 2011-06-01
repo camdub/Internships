99b0fb02ea0360285162c304be4fb0776cf16a40
Kory Calmes <kory@calmes.org>
2011-06-01 12:38:22 -0600

    added JSON for index.  JSON for individual record coming later.

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -3,10 +3,63 @@ class InternshipsController < ApplicationController
   # GET /internships.xml
   def index
     @internships = Internship.all
+    
+    
+    
 
+    # Very inefficient when users table has thousands of rows.
+      
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @internships }
+      format.json  {
+        internships = Hash.new
+        
+        @internships.each do |internship|
+          internship[:provider] = internship.provider
+          internship[:locations] = internship.locations
+          internship[:languages] = internship.languages
+          internship[:fields] = internship.fields
+          
+          #this is alternate and cleaner, but a lot more work.  might be worth it for the project... might not...
+          
+          
+          
+          internship.locations.each do |location|
+            internships[location.country.un_code] = Array.new if internships[location.country.un_code] == nil
+            internships[location.country.un_code] << {
+              'id' => internship.id, 
+              'name' => internship.name, 
+              'provider_name' => internship.provider.name,
+              'city' => location.city, 
+              'state' => location.state.name, 
+              'country' => location.country.name, 
+            }
+          end
+          /
+          internships << {
+              'id' => internship.id, 
+              'name' => internship.name, 
+              'provider_name' => internship.provider.name, 
+              'locations' => Array.new 
+          }
+          internship.locations.each do |location|
+            internships.last['locations'] << {
+              'city' => location.city, 
+              'state' => location.state.name, 
+              'country' => location.country.name, 
+              'country_code' => location.country.un_code 
+            }
+          end
+          /
+          
+          #ends here, use with render :json => internships
+          #this second one would be better for giving short info at first, but the other would port ALL of the data on load, pros and cons
+        end
+        #render :json => @internships
+        render :json => internships
+        
+      }
     end
   end
 
@@ -18,6 +71,7 @@ class InternshipsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @internship }
+      format.json { render :json => @internship }
     end
   end
 
