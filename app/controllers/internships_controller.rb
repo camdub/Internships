c631592ba706326205a896699a42831b4da9fc12
Kory Calmes <kory@calmes.org>
2011-06-02 09:57:35 -0600

    set the JSON for a specific internship

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -36,22 +36,6 @@ class InternshipsController < ApplicationController
               'country' => location.country.name, 
             }
           end
-          /
-          internships << {
-              'id' => internship.id, 
-              'name' => internship.name, 
-              'provider_name' => internship.provider.name, 
-              'locations' => Array.new 
-          }
-          internship.locations.each do |location|
-            internships.last['locations'] << {
-              'city' => location.city, 
-              'state' => location.state.name, 
-              'country' => location.country.name, 
-              'country_code' => location.country.un_code 
-            }
-          end
-          /
           
           #ends here, use with render :json => internships
           #this second one would be better for giving short info at first, but the other would port ALL of the data on load, pros and cons
@@ -65,13 +49,85 @@ class InternshipsController < ApplicationController
 
   # GET /internships/1
   # GET /internships/1.xml
+  # GET /internships/1.json
   def show
     @internship = Internship.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @internship }
-      format.json { render :json => @internship }
+      format.json {
+        internship = {
+          'name' => @internship.name, 
+          'is_paid' => @internship.is_paid,
+          'is_full_time' => @internship.is_full_time, 
+          'is_part_time' => @internship.is_part_time, 
+          'stipend' => @internship.stipend,
+          'description' => @internship.description,
+          'qualifications' => @internship.qualifications,
+          'qualifications_academic' => @internship.qualifications_academic,
+          'for_credit' => @internship.for_credit,
+          'requires_us_citizenship' => @internship.requires_us_citizenship,
+          'application_process' => @internship.application_process,
+          'deadline' => @internship.deadline,
+          'academic_contact_name' => false,
+          'provider_contact_name' => false,
+          'locations' => Array.new,
+          'academic_focuses' => Array.new,
+          'fields' => Array.new,
+          'languages' => Array.new,
+          'financial_assistance_options' => Array.new,
+          'semesters' => Array.new
+        }
+        #These 2 fields are one or the other, so they are set here on thier own so we avoid calling 'name' on an nil object
+        internship['academic_contact_name'] = @internship.academic_contact.name if @internship.academic_contact != nil
+        internship['provider_contact_name'] = @internship.provider_contact.name if @internship.provider_contact != nil
+        
+        @internship.locations.each do |location|
+          internship['locations'] << {
+            'city' => location.city, 
+            'state' => location.state.name, 
+            'country' => location.country.name
+          }
+        end
+        @internship.academic_focuses.each do |academic_focus|
+          internship['academic_focuses'] << {
+            'name' => academic_focus.name, 
+            'type' => academic_focus.academic_focus_type.name
+          }
+        end
+        @internship.fields.each do |field|
+          internship['fields'] << {
+            'name' => academic_focus.name,
+            'industry' => academic_focus.industry.name
+          }
+        end
+        @internship.languages.each do |language|
+          internship['languages'] << {
+            'name' => language.name
+          }
+        end
+        @internship.financial_assistance_options.each do |financial_assistance_option|
+          internship['financial_assistance_options'] << {
+            'name' => financial_assistance_option.name,
+            'type' => financial_assistance_option.financial_assistance_option_type.name,
+            'how_much' => financial_assistance_option.how_much,
+            'qualifications' => financial_assistance_option.qualifications,
+            'source' => financial_assistance_option.source,
+            'website' => financial_assistance_option.website            
+          }
+        end
+        @internship.semesters.each do |semester|
+          internship['semesters'] << {
+            'name' => semester.name
+          }
+        end
+        
+        
+        
+
+        render :json => internship 
+      }
     end
   end
 
