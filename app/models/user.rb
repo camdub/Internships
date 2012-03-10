71b64d0b31d51c64755a61afa2a727c2542b29d4
Kory Calmes <kory@calmes.org>
2012-03-09 18:58:08 -0700

    Fixed bug with passenger
    Bug was exhibited with passenger but not on thin with heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   
   def display_name
       if not self.firstname.blank?
-        "#{self.firstname self.lastname}"
+        self.firstname << " " << self.lastname
       else
         "#{self.net_id}"
       end
