5623088c9a511e83c92762bd86fac53c68cfabca
Kory Calmes <kory@calmes.org>
2012-04-13 14:48:45 -0600

    Installed Rails 3.2
    Fresh install of rails 3.2.  Now pieces can be added back in as Engines.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Internships::Application.config.secret_token = '3c4512e1d631b98fa4ec080d53c2dbcd404ae8f5175241fd1ecb810f85a1f43652d4d1e5bf3db47ae7cf3b376cb371720f2da80108aef7910f5621f89ab6ce6c'
